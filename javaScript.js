79dc1287bd30ad235aa93826f707324fa7a41d17
khalidbhaarat <khalidbhaarat@gmail.com>
2022-07-15 10:41:51 -0400

    final upload

--- javaScript.js
+++ javaScript.js
@@ -1,26 +1,31 @@
-const sliderContainer = document.querySelector('slider-container')
-const slideRight =document.querySelector('.right-slide')
-const slideLeft= document.querySelector('.left-slide')
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slideLength = slideRight.querySelectorAll('div').length
+const slidesLength = slideRight.querySelectorAll('div').length
 
-let activeSlideIndex=0
+let activeSlideIndex = 0
 
-slideLeft.style.top =`-${(slideLength-1)*100}vh`        
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
-upButton.addEventListener('click',() => changeSlide('up'))
-downButton.addEventListener('click',() => changeSlide('down'))
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
 
-
-const changeSlide= (direction) => {
-    const sliderHeight= sliderContainer.clientHeight
-    if(direction === 'up'){
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
         activeSlideIndex++
-        if(activeSlideIndex > slideLength -1){
-            activeSlideIndex=0
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
         }
     }
-    slideRight.style.transform= `translateY(-${activeSlideIndex * sliderHeight}px)`
-    slideRight.style.transform= `translateY(${activeSlideIndex * sliderHeight}px)`
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
 }
